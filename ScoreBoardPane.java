323df235dfb71c62efcb2c1e99539dce0300736e
saramberr12 <higginssarah12@gmail.com>
2017-05-03 14:50:18 -0400

    ScoreBoardPane
    This is the scoreboard pane with dummy data in it

--- ScoreBoardPane.java
+++ ScoreBoardPane.java
@@ -15,71 +15,95 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
-
-import javax.security.auth.callback.Callback;
+import javafx.util.Callback;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
-
 /**
  * Created by sarah on 5/2/17.
+ * @author Sarah Higgins and Ying Sun
+ * ScoreBoardPane class is the class the displays the Scoreboard from the Main GUI.
+ * It shows all of the Player's names and their scores.
  */
 public class ScoreBoardPane extends Application {
 
-    //attributes contributed by Ying
+    /**
+     * attributes contributed by Ying Sun
+     */
     private static Map<String, Integer> scores = new HashMap<String, Integer>();
     private static final int MAX_PLAYER_NUMBER = 16;
 
-    //attributes contributed by Sarah
-    //private TableView table = new TableView();
-    public static final String Column1MapKey = "A";
-    public static final String Column2MapKey = "B";
+    /**
+     * attributes contributed by Sarah Higgins
+     */
+    public static final String Column1MapKey = "PlayerName";
+    public static final String Column2MapKey = "Player Score";
 
+    /**
+     * ScoreBoardPane constructor
+     */
     public ScoreBoardPane() {
 
     }
 
+    /**
+     * main() method launches the program
+     * @param args takes in all of the javaFX arguments withing
+     *             the start() method
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * creates the GUI
+     * @param stage
+     */
     @Override
     public void start(Stage stage) {
         Scene scene = new Scene(new Group());
         stage.setTitle("March Madness Bracket");
-        stage.setWidth(300);
-        stage.setHeight(500);
+        stage.setWidth(310);
+        stage.setHeight(520);
 
         final Label label = new Label("March Madness");
         label.setFont(new Font("Arial", 20));
 
+        /**
+         * TableColumn userNameCol is the column on the left side of the table
+         */
         TableColumn<Map, String> userNameCol = new TableColumn<>("Username");
-        userNameCol.setMinWidth(130);
-        userNameCol.setMaxWidth(130);
-        userNameCol.setCellFactory(new MapValueFactory(Column1MapKey));
+        userNameCol.setMinWidth(140);
+        userNameCol.setMaxWidth(140);
+        userNameCol.setStyle("-fx-border-width: 3px");
+        userNameCol.setCellValueFactory(new MapValueFactory(Column1MapKey));
+        userNameCol.setSortable(false);
+        userNameCol.setSortType(TableColumn.SortType.DESCENDING); //sorts column from highest to lowest
 
-
+        /**
+         * TableColumn totalPtsCol is the column on the right side of the table
+         */
         TableColumn<Map, String> totalPtsCol = new TableColumn<>("Total Points");
-        totalPtsCol.setMinWidth(130);
-        totalPtsCol.setMaxWidth(130);
-        totalPtsCol.setCellFactory(new MapValueFactory(Column2MapKey));
+        totalPtsCol.setMinWidth(140);
+        totalPtsCol.setMaxWidth(140);
+        totalPtsCol.setStyle("-fx-border-width: 3px");
+        totalPtsCol.setCellValueFactory(new MapValueFactory(Column2MapKey));
+        totalPtsCol.setSortable(false);
+        totalPtsCol.setSortType(TableColumn.SortType.DESCENDING); //sorts column from highest to lowest
 
-
-        TableView table_view = new TableView<>();
-
+        /**
+         * TableView table_view is what the user sees in the GUI. This creates the table.
+         */
+        TableView table_view = new TableView<>(generateDataInMap());
         table_view.setEditable(true);
         table_view.getSelectionModel().setCellSelectionEnabled(true);
         table_view.getColumns().setAll(userNameCol, totalPtsCol);
-        /*
-        javafx.util.Callback<TableColumn<Map, String>, TableCell<Map, String>>
-                cellFactoryForMap = new javafx.util.Callback<TableColumn<Map, String>,
-                TableCell<Map, String>>() {
+        Callback<TableColumn<Map, String>, TableCell<Map, String>> cellFactoryForMap = new Callback<TableColumn<Map, String>, TableCell<Map, String>>() {
             @Override
             public TableCell call(TableColumn p) {
                 return new TextFieldTableCell(new StringConverter() {
                     @Override
-                    public String toString() {
+                    public String toString(Object t) {
                         return t.toString();
                     }
                     @Override
@@ -91,11 +115,10 @@ public class ScoreBoardPane extends Application {
         };
         userNameCol.setCellFactory(cellFactoryForMap);
         totalPtsCol.setCellFactory(cellFactoryForMap);
-        */
 
         final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.setSpacing(6);
+        vbox.setPadding(new Insets(12, 0, 0, 12));
         vbox.getChildren().addAll(label, table_view);
 
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
@@ -104,23 +127,23 @@ public class ScoreBoardPane extends Application {
         stage.show();
     }
 
-    /*
+
     private ObservableList<Map> generateDataInMap() {
         int max = 16;
         ObservableList<Map> allData = FXCollections.observableArrayList();
         for (int i = 1; i < max; i++) {
-            Map<String, String> dataRow = new HashMap<>();
+            Map<String, String> scoresRow = new HashMap<>();
 
-            String value1 = "A" + i;
-            String value2 = "B" + i;
+            String value1 = "player" + i;
+            String value2 = "score" + i;
 
-            dataRow.put(Column1MapKey, value1);
-            dataRow.put(Column2MapKey, value2);
+            scoresRow.put(Column1MapKey, value1);
+            scoresRow.put(Column2MapKey, value2);
 
-            allData.add(dataRow);
+            allData.add(scoresRow);
         }
         return allData;
-    } */
+    }
 
     //Ying's code, method addPlayer adds a player to the Bracket
     public void addPlayer(String name, int score) {
