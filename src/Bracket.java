cca41a965197756ad51e90693c196b3f942d5f9c
Hillary <h_ssemakula@post.massbay.edu>
2017-05-05 23:16:19 -0400

    Update Bracket.java

--- src/Bracket.java
+++ src/Bracket.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 /**
  * Bracket Class
  * Created by Matt and Dan on 5/1/2017.
+ * Contributor: Hillary Ssemakula 5/1
  */
-/**Hillary Ssemakula 5/1 */
 public class Bracket implements Serializable //Hillary: This bracket class is to implement the serializable interface inorder to be serialized
 {
     //Attributes
