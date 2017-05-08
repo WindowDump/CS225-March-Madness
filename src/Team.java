3acef8b2cd6b11d42e07e9b1121707c6ce64bfbe
TylerPags <t_pagliazzo@post.massbay.edu>
2017-05-07 21:45:33 -0400

    Add files via upload

--- src/Team.java
+++ src/Team.java
@@ -1,42 +1,107 @@
+/**
+ * 
+ * @author Shivanie and Tyler
+ * @description 
+ *       Team class holds all the information for individual teams. Their name, info, and ranking.
+ *       
+ */
+
 public class Team{
-  
+
   private String name;
   private String info;
   private int ranking;
-  /*
-   * can be implemented if we have time.
-   * public double defense;
-   * public double offense;
-   */ 
+  public double offensePPG;
+  public double defensePPG;
   
-  
-  public Team(String name, String info, int ranking){
+  /**
+   * Constructor
+   * @param name 
+   *        The name of the team
+   * @param info
+   * 		A short description of the team
+   * @param ranking
+   * 		The ranking in the team region from 1 to 16
+   */
+  public Team(String name, String info, int ranking, double oPPG, double dPPG){
     this.name = name;
     this.info = info;
     this.ranking = ranking;
+    offensePPG = oPPG;
+    defensePPG = dPPG;
   }
     
-  
+  /**
+   * 
+   * @return name
+   */
   public String getName(){
     return name;
   }
+  
+  /**
+   * 
+   * @return info
+   */
   public String getInfo(){
     return info;
   }
+  
+  /**
+   * 
+   * @return ranking
+   */
   public int getRanking(){
     return ranking;
   }
-  /*public double getOffense(){
-    return offense;
+  
+  /**
+   * 
+   * @return offensePPG
+   */
+  public double getOffensePPG(){
+    return offensePPG;
   }
-  public double getDefense(){
-    return defense;
+  
+  /**
+   * 
+   * @return defensePPG
+   */
+  public double getDefensePPG(){
+    return defensePPG;
   }
-  */
+  
+  /**
+   * 
+   * @param info 
+   * 		The short description of the team
+   */
   public void setInfo(String info){
     this.info = info;
   }
+  
+  /**
+   * 
+   * @param ranking
+   * 		The ranking from 1 to 16
+   */
   public void setRanking(int ranking){
     this.ranking = ranking;
+  }
+  
+  /**
+   * 
+   * @param newDefense The new points per game for defense
+   */
+  public void setDefense(double newDefense){
+	  defensePPG = newDefense;
+  }
+  
+  /**
+   * 
+   * @param newOffense the new points per game for offense.
+   */
+  public void setOffense(double newOffense){
+	  offensePPG =  newOffense;
   }
 }
